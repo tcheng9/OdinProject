72d959ce8e13fbdaa5963675dfb13863faa5d0b4
Tommy C <tcheng789@gmail.com>
2022-01-28 16:53:38 -0600

    Uploading for the day, figured out how to actualy make it do math but need to finish implementing

--- Calculator/script.js
+++ Calculator/script.js
@@ -76,6 +76,7 @@ buttons.forEach(function(item){
 //ANSWER: already done, use .forEach(item) => console.log(item.value);
 
 //Step 5: Set a event that register only on clicks and updates the page
+/*
 const buttons = document.querySelectorAll("input");
 
 buttons.forEach(function(item){
@@ -84,3 +85,161 @@ buttons.forEach(function(item){
         display.textContent = stringHolder;
     })
 })
+*/
+/*
+function operations(val){
+    //if val == addition, then do addition with some sort of string holder
+
+    //if val == subtraction then do subtraction
+
+    //if val == multiply,
+
+    //if val == divide
+
+    //
+
+}
+*/
+///////////////////////////////////////////////////////////////////
+//Task - Just make addition work
+
+//Steps: 
+//Step 1: Get first number string
+//Step2: Recognize add operation
+//Step 3: Get the second number string
+//Step 4: return summed output to the display  when enter is hit
+//OR return back to string so it can continued to be used
+
+
+
+/*
+PSEUDOCODE
+while button.value != "="
+    if button != (+, -, *, /)
+        string1.append(button.value)
+    else (button == (+, -, *, /))
+        string2 += string1
+        string = ""
+
+    (HOW TO ADD STIRNG 1 AND STRING2 WHEN THERE ARE VALUES INSIDE AND DON'T)
+    (HOW TO DETECT IF BOTH VALUES HAVE A VARIABLE)
+        SOLUTION: "(&& STR1 != "")""
+
+*/
+
+//First attempt for calculator actually calculate
+
+let str1 = "";
+let str2 = "";
+let sum = 0;
+//NOTE: Do need to deal with type error as I processed but w/e
+
+//PSEUDOCODE/CODE
+/*
+        while (item.value != "=")
+        {
+            if (item.value != "[+, -, *, /]")
+            {
+                str2 = str1 //str2 holds the value
+                str1 = "" // so it can hold a new string value
+                sum = str1 + str2 //BUT how to determine the 2 strings
+            } elif (item.value === "0-9")
+            {
+                str1.append(string)
+                
+            }
+        }
+*/
+
+//END PSEUDOCODE
+//REFLECTIONS: 2 variables does not seem right
+//Maybe processing a long string
+/***************
+ //SECOND ATTTEMPT ////////////////////////////
+while str != "=":
+        if str = +, -, /, +:
+            do corresponding operation
+    
+// Maybe string split? => Provides an array 
+        for i in array:
+            if arr[i] "function":
+                if arr[i[] == 'add" 
+                    //Do addiotn
+                
+                returnSum = returnSum + arr[i+1]
+            if arr[i] != function
+                continue
+///SECOND ATTEMPT END////////////////////////////
+*///////////
+
+//Actually implementing second attempt
+const buttons = document.querySelectorAll("input");
+
+//Semi-pseudo code in one go
+buttons.forEach(function(item){
+    item.addEventListener("click", function(){
+        stringHolder += item.value;
+        display.textContent = stringHolder;
+        let returnArr = [];
+        let returnSum = 0;
+        if (item.value === "=") 
+        {
+            //returnArr = stringHolder.split("/[+-*/]/");
+            returnArr = stringHolder.split(/[+,-,*, /, = ]/);
+            console.log(returnArr);
+            //Loop through array, if arr[i] = special symbol then??
+                //Initial value = arr[i]
+                //OFFSET THE START OF THE LOOP!!!! num1 = arr[0], if arr[1] = "+", then arr[1+1] = num 2, etc.
+            let returnSum = returnArr[0];
+
+            for (var i = 0; i < returnArr.length - 1; i++)
+            {
+                try
+                {
+                    returnArr[i] = parseInt(returnArr[i]);
+                } 
+
+                catch(err)
+                {
+                    Console.log("Not a num");
+                }
+                
+            }
+            /*
+            for (var i = 1; i < returnArr.length - 1; i++)
+            {
+                if (returnArr[i] === "+")
+                {
+                    add(returnSum, returnArr[i+1]);
+                }
+
+                if (returnArr[i] === "-")
+                {
+                    subtract(returnSum, returnArr[i+1]);
+                }
+
+                if (returnArr[i] === "*")
+                {
+                    multiply(returnSum, returnArr[i+1]);
+                }
+
+                if (returnArr[i] === "/")
+                {
+                    divide(returnSum, returnArr[i+1]);
+                }
+
+            }
+            */
+        }
+
+        if (item.value === "C")
+        {
+            returnArr = [];
+            stringHolder = "";
+            display.textContent = "Ready for new values";
+        }
+})
+
+
+})
+
