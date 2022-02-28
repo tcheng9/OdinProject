370955d786ef252669ecdccc3d55c591cbb4e9ac
Tommy <tcheng789@gmail.com>
2022-02-27 23:54:56 -0600

    Updates for tonight

--- library/script.js
+++ library/script.js
@@ -10,6 +10,11 @@ function bookMaker(title, author, length, read){
     this.read = read;
 }
 
+function addBook(book){
+    bookArr.push(book);
+}
+
+
 let headers = ['title', 'author', 'page count', 'read?'];
 
 let book1 = new bookMaker("harry potter 1", "jk rowling", 342, true);
@@ -44,34 +49,70 @@ function createTable(){
 
     bookArr.forEach( emp => {
         let row = document.createElement('tr');
+        row.className = counter;
 
         Object.values(emp).forEach(text => {
             let cell  = document.createElement('td');
             let textNode = document.createTextNode(text);
-            cell.className = counter;
             cell.appendChild(textNode);
             row.appendChild(cell);
-            row.appendChild(btn);
+            
         })
+
+        //Function to create buttons -> ISSUE: nested within the bookArr loop. I should make my another loop for this
+        let cell = document.createElement('td');
+        let btn = document.createElement("input");
+        btn.type = "button";
+        btn.className = "btn" + counter;
+        btn.value = 451;
+
+        cell.append(btn);
+        
+
+        row.appendChild(btn);
         counter = counter + 1;
         table.appendChild(row);
-        
+       
 
     });
 
-    
-    
     myTable.appendChild(table);
-
-    
-    
 }
-// var btn = document.createElement('input');
-// btn.type = "button";
-// btn.className = "btn";
-// btn.value = "451";
+
+
 
 
 // row.appendChild(btn);
 createTable();
 
+//When button is clicked, I want it to run a functionto run a popup form
+
+// var form = document.getElementById("formPopUp");
+// btn.addEventListener("click", openForm());
+
+
+function openForm(){
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm(){
+    document.getElementById("myForm").style.display = "none";
+}
+
+//On form submit click, run this function
+
+/* Document select all form values and run functions to turn
+// into a book object and append to array and remake table
+*/
+function onSubmit(){
+    let formAuthor = document.myForm.author.value;
+    let formTitle = document.myForm.title.value;
+    let formPages = document.myForm.pages.value;
+    let formReadStatus = document.myForm.readStatus.value;
+
+    let book = bookMaker(formAuthor, formTitle, formPages, formReadStatus);
+    
+    
+    addBook(book);
+    createTable();
+}
