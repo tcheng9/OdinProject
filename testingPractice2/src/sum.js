107eef7d40c5175f8d27a3c24c482828356d68b6
Tommy-W10 <tcheng789@gmail.com>
2022-04-07 21:05:36 -0500

    nightly update

--- testingPractice2/src/sum.js
+++ testingPractice2/src/sum.js
@@ -12,3 +12,115 @@ function sum(a, b) {
 
 module.exports.sum = sum;
 
+function capitalize(string){
+  let returnStr = string.charAt(0).toUpperCase();
+  
+ 
+  for (let i = 1; i < string.length; i++){
+    returnStr += string[i]; 
+  }
+
+  return returnStr;
+}
+
+module.exports.capitalize = capitalize;
+
+function reverseString(string){
+  let returnStr = "";
+  for (let i = string.length - 1; i >= 0; i--){
+    returnStr += string[i];
+  }
+  return returnStr;
+}
+
+module.exports.reverseString = reverseString;
+
+const calculator = (() => {
+  const add = (a, b) => a + b;
+  const subtract = (a, b) => a - b;
+  const multiply = (a, b) => a * b;
+  const divide = (a, b) => a / b;
+  return {
+    add,
+    subtract,
+    multiply,
+    divide,
+  };
+})();
+
+module.exports.calculator = calculator;
+
+
+//Function caesarCipher -> take a string and shift the characters
+/*
+Notes:
+1. Get int value of a letter
+2. Int value add 1 
+3. Int value -> letter
+
+Z -> A conversion: if this specific conversion, convert to A
+
+QUESTION: How to account for int value of capitals and lowercase
+for "z" and "Z", look for uppercase/lowercase "z' and hardset it
+
+//Code for turning letter into ascii values
+//From Letter to Int
+s.charCodeAt(0) - 97; //Get int at string index 0
+
+//Converting from int to letter
+String.fromCharCode(97 + n); //n is the amount to add to get the letter you want
+
+
+//Potential solution 1
+1.Loop through string
+2.Check if string if uppcase or lowercase
+3a. If uppercase, do necessary conversions
+3b. if lowercase, do necssary conversions
+4. append to returnStr
+5. Finish loop and return string
+
+
+//Note: it's not just Z -> A, I need to loop around -> set a boundary, if value + increment > 25, set to 0 then add or something
+Site for calculating caesarcipher: 
+http://practicalcryptography.com/ciphers/caesar-cipher/
+
+
+Key parts:
+1. Check for upper/lowercase
+2. Loop to convert to int AND converting back to string
+3. Increment string as needed
+*/
+
+const caesarCipher = ((string, increment) => {
+  let returnStr = "";
+  function isUpperCase(ch){
+    if (!isNaN(ch * 1)){
+      return 'ch is numeric';
+    } else {
+      if (ch == ch.toUpperCase()){
+        return true;
+      } 
+
+      if (ch == ch.toLowerCase()){
+        return false;
+      }
+    }
+  }
+
+  function convertString(){   
+    for (let i =0 ;i < string.length; i++) {
+      if (isUpperCase(string[i]) == true){
+        //Convert uppcase string to uppercase letter
+      } else {
+        //convert lowercase string to lowercase letter
+      }
+    }
+  } 
+
+  function incrementer(){
+
+  }
+
+})();
+
+//Stopped here: How to increment letter in an automatic way?
--- testingPractice2/src/sum.test.js
+++ testingPractice2/src/sum.test.js
@@ -1,13 +1,56 @@
 var sum = require('./sum').sum
 var subtract = require('./sum').subtract
+var capitalize = require('./sum').capitalize;
+var reverseString = require('./sum').reverseString;
 
+//Sum test
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-
-
-
+//Subtract test
 test("subtract 2 - 1", () => {
   expect(subtract(2,1)).toBe(1);
 });
+
+//Capitlize test
+test("capitalize the first letter of a string", () => {
+  expect(capitalize("alphabet")).toBe("Alphabet");
+})
+
+//Reverse a string test
+
+test("reverse a string", () => {
+  expect(reverseString("Alphabet")).toBe("tebahplA");
+})
+
+//Write a calculator object that can handle add, subtract, divide, multiply
+//FIRST: figure out how to write a object that will have these functions
+//AND how to call it
+var calculator = require('./sum').calculator;
+
+//Calc- Sum
+test("add value", () => {
+  expect(calculator.add(1,2)).toBe(3);
+})
+
+//Calc - Subtract
+test("subtract value", () => {
+  expect(calculator.subtract(2,1)).toBe(1);
+})
+
+//Calc - Multiply
+test("multiply value", () => {
+  expect(calculator.multiply(2,1)).toBe(2);
+})
+
+//Calc Divide
+test("divide value", () => {
+  expect(calculator.divide(4, 2)).toBe(2);
+})
+
+//Caesarcipher 
+
+test("caesarcipher", () => {
+  expect(caesarCipher("abc", 11)).toBe("lmn")
+})
