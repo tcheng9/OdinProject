0320ad77437dc364139953619f53c0a974ac2290
Tommy C <tcheng789@gmail.com>
2022-05-31 15:41:52 -0500

    afternoon break update

--- memory-card/src/App.js
+++ memory-card/src/App.js
@@ -1,7 +1,8 @@
-
+import React from "react";
 import './App.css';
 import Header from "./components/header/header"
 import Pictures from './components/pictures/pictures';
+
 function App() {
   return (
     <div className="App">
--- memory-card/src/components/pictures/pictures.js
+++ memory-card/src/components/pictures/pictures.js
@@ -1,11 +1,20 @@
 import React from "react";
-import Bunny from "./images/bunny.jpg"
-
-console.log(Bunny);
+import cat from "./images/cat.jpg"
+import bunny from "./images/bunny.jpg"
+import deer from "./images/deer.jpg"
+import dogs from "./images/dogs.jpg"
+import dolphin from "./images/dolphin.jpg"
+import hedgehog from "./images/hedgehog.jpg"
+import jellyfish from "./images/jellyfish.jpg"
+import lion from "./images/lion.jpg"
+import panda from "./images/panda.jpg"
+import parrot from "./images/parrot.jpg"
+import penguin from "./images/penguin.jpg"
+import squirrel from "./images/squirrel.jpg"
 
 const Pictures = () => {
     return (
-        <img src = {Bunny}> </img>
+        <img src = {squirrel} width = "200px" height = "200px" alt = "Bunny" />
     )
 }
 
