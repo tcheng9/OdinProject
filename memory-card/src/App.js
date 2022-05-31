d833f25dfed1d551e4936c7efe5a073473050b7d
Tommy <tcheng789@gmail.com>
2022-05-31 13:50:11 -0500

    update

--- memory-card/src/App.js
+++ memory-card/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
 import Header from "./components/header/header"
+import Pictures from './components/pictures/pictures';
 function App() {
   return (
     <div className="App">
       <Header />
+      <Pictures />
     </div>
   );
 }
--- memory-card/src/components/pictures/pictures.js
+++ memory-card/src/components/pictures/pictures.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Bunny from "./images/bunny.jpg"
+
+console.log(Bunny);
 
 const Pictures = () => {
-
+    return (
+        <img src = {Bunny}> </img>
+    )
 }
 
 export default Pictures
