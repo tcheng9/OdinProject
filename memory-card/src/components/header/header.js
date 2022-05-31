d31e308bdd9b589f5e024d9823d9485c7d005b48
Tommy <tcheng789@gmail.com>
2022-05-31 09:14:16 -0500

    update

--- memory-card/src/components/header/header.js
+++ memory-card/src/components/header/header.js
@@ -3,7 +3,10 @@ import React, {useState} from 'react';
 const Header = () => {
     return (
         <div>
-            Test
+            <h1> Game title </h1>
+            <p> Explanation of rules</p>
+            <p> Current score placeholder </p>
+            <p> Maximum score </p>
         </div>
     )
 }
