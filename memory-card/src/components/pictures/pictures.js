8ea119626ec7c994da1d6f6912c52f3fcfa83c3f
Tommy <tcheng789@gmail.com>
2022-06-02 19:05:17 -0500

    checkpoint, got shuffler working

--- memory-card/src/components/pictures/pictures.js
+++ memory-card/src/components/pictures/pictures.js
@@ -11,33 +11,106 @@ import panda from "./images/panda.jpg"
 import parrot from "./images/parrot.jpg"
 import penguin from "./images/penguin.jpg"
 import squirrel from "./images/squirrel.jpg"
+import { useEffect, useState } from "react";
 
-const Pictures = () => {
-    const picturesArr = [
+const picturesArr = [
     {
         id: 1,
-        photo: {cat},
+        src: cat,
         text: "Cat",
     },
     {
         id: 2,
-        photo: {bunny},
+        src: bunny,
         text: "bunny"
     }, 
     {
         id: 3, 
-        photo: {deer},
+        src: deer,
         text: "deer"
+    },
+    {
+        id: 4, 
+        src: dogs,
+        text: "dogs"
+    },
+    {
+        id: 5, 
+        src: dolphin,
+        text: "dolphin"
+    },
+    {
+        id: 6, 
+        src: hedgehog,
+        text: "hedgehog"
+    },
+    {
+        id: 7, 
+        src: jellyfish,
+        text: "jellyfish"
+    },
+    {
+        id: 8, 
+        src: lion,
+        text: "lion"
+    },
+    {
+        id: 9, 
+        src: panda,
+        text: "panda"
+    },
+    {
+        id: 10, 
+        src: parrot,
+        text: "parrot"
+    },
+    {
+        id: 11, 
+        src: penguin,
+        text: "penguin"
+    },
+    {
+        id: 12, 
+        src: squirrel,
+        text: "squirrel"
     }
     ]
 
+const Pictures = () => {
+    const [list, setList] = useState(picturesArr);
+
+    function shuffle(arr){
+        var ctr = arr.length,
+            temp,
+            index;
+
+        while (ctr > 0){
+            index = Math.floor(Math.random() * ctr);
+            ctr--;
+            temp = arr[ctr];
+            arr[ctr] = arr[index];
+            arr[index] = temp;
+        }
+        return arr;
+    }
+
+    function handleSubmit(){
+        const changes = shuffle([...list]);
+        setList(changes);
+        console.log("shuffled");
+    }
+
     return (
-        // <div>
-        // {/* <img src = {squirrel} width = "200px" height = "200px" alt = "Bunny" /> */}
-        //     {picturesArr.map((picture) => (
-        //         <img src = {picture.photo} key = {picture.id}></img> 
-        //     ))}
-        // </div>
+       
+        <div className = "picturesContainer">
+            {list.map((index) => 
+            <div key = {index.id}>    
+            <img onClick = {handleSubmit} src = {index.src} title = {index.text} alt = "Images" height = "200px" width = "200px"/>
+            <p>{index.text}</p> 
+            </div>
+            )}
+            <button onClick = {handleSubmit}> test </button>
+        </div>
     )
 }
 
