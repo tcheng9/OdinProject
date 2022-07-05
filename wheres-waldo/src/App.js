586ade47f7362165c10656f39ea0873f861d63cb
Tommy C <tcheng789@gmail.com>
2022-07-05 12:45:56 -0500

    updating to afternoon break

--- wheres-waldo/src/App.js
+++ wheres-waldo/src/App.js
@@ -1,9 +1,14 @@
-
+import {Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       Hello from App
+      <ul>
+        <li>
+          <Link to = "/WaldoMain"> Waldo Page </Link>
+        </li>
+      </ul>
     </div>
   );
 }
--- wheres-waldo/src/components/RouteSwitch.js
+++ wheres-waldo/src/components/RouteSwitch.js
@@ -1,7 +1,7 @@
 
 
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import App from "./App";
+import App from "../App";
 import Home from './Home';
 import Hiscores from './Hiscores';
 import WaldoMain from "./WaldoMain";
