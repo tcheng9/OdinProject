2722b1d44d224c79c43d16d1d313c9c16c23674a
Tommy C <tcheng789@gmail.com>
2022-07-10 14:29:31 -0500

    update

--- wheres-waldo/src/components/WaldoMain.js
+++ wheres-waldo/src/components/WaldoMain.js
@@ -1,11 +1,50 @@
 import {Link} from 'react-router-dom';
 import waldo from '../waldo_beach.jpg';
 import './WaldoMain.css';
+import {useState, useEffect} from 'react';
 
 const WaldoMain = () => {
+    const [x, setX] = useState();
+    const [y, setY] = useState();
+    
+    function setXY() {
+        let circleDiv = document.getElementById('CursorCircle');
+        circleDiv.style.positoin = "absolute";
+        circleDiv.style.left = {x} + 'px';
+        circleDiv.style.top = {y} + 'px';
+        console.log('setting circle coords');
+    }
+    // const setXY = (x,y) => {
+    //     var x = 0;
+    //     var y = 0;
+    //     let circleDiv = document.getElementById('CursorCircle');
+    //     circleDiv.style.positoin = "absolute";
+    //     circleDiv.style.left = x + 'px';
+    //     circleDiv.style.top = y + 'px';
+    //     console.log('setting circle coords');
+    // }
+
+    useEffect( () => {
+        const update = (e) => {
+            setX(e.x);
+            setY(e.y);
+
+        }
+        // window.addEventListener('click', setXY(x,y));
+        window.addEventListener('click', update);
+       
+    },
+        [setX,setY]
+        
+    )
+
+    
+
     return (
         <div>
-            
+            <div>
+                {x}-----------{y}
+            </div>
             <img src = {waldo} /> 
             <ul>
                 <li>
