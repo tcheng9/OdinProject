06215121aea8a3b5a1a628ce4855e3fea68cf3e3
Tommy C <tcheng789@gmail.com>
2022-07-12 13:26:55 -0500

    update

--- wheres-waldo/src/components/WaldoMain.js
+++ wheres-waldo/src/components/WaldoMain.js
@@ -8,44 +8,42 @@ const WaldoMain = () => {
     const [y, setY] = useState();
     
     function setXY() {
+        
         let circleDiv = document.getElementById('CursorCircle');
-        circleDiv.style.positoin = "absolute";
+        circleDiv.style.position = "absolute";
         circleDiv.style.left = {x} + 'px';
         circleDiv.style.top = {y} + 'px';
         console.log('setting circle coords');
     }
-    // const setXY = (x,y) => {
-    //     var x = 0;
-    //     var y = 0;
-    //     let circleDiv = document.getElementById('CursorCircle');
-    //     circleDiv.style.positoin = "absolute";
-    //     circleDiv.style.left = x + 'px';
-    //     circleDiv.style.top = y + 'px';
-    //     console.log('setting circle coords');
-    // }
 
-    useEffect( () => {
-        const update = (e) => {
-            setX(e.x);
-            setY(e.y);
+    const setCircle = (e) =>  {
+        let circleDiv = document.getElementById('CursorCircle');
+        circleDiv.style.position = "absolute";
+        circleDiv.style.left = (e.screenX - 50) + 'px';
+        circleDiv.style.top = (e.screenY - 150) + 'px';
+        console.log("Original" + e.screenX + ' ----------' + e.screenY);
+        console.log("X coordinate: " + (e.screenX - 50));
+        console.log("Y coordinate: " + (e.screenY - 150));
+        checkWaldo(e, e.screenX, e.screenY);
+    }
 
-        }
-        // window.addEventListener('click', setXY(x,y));
-        window.addEventListener('click', update);
-       
-    },
-        [setX,setY]
+    const checkWaldo = (e,x,y) => {
+        //Waldo's coordinates
+        let waldoCoords = [100, 100]; 
+
+        //Get coordinates from setCircle/ on trigger. Don't use screen listener
         
-    )
-
+        //If x and y are within range of waldo's coordinates, trigger a popup that lets user input their name
+        if ((x < waldoCoords[0] + 100 && x > waldoCoords[0] - 100) && (y > waldoCoords[1] - 100 && y < waldoCoords[1] + 100)){
+            console.log("correct coordinates")
+        }
+    }
     
 
     return (
         <div>
-            <div>
-                {x}-----------{y}
-            </div>
-            <img src = {waldo} /> 
+            
+            <img src = {waldo} onClick = {setCircle}/> 
             <ul>
                 <li>
                 <Link to = "/"> App Main Page </Link>
@@ -54,9 +52,13 @@ const WaldoMain = () => {
                 <Link to = "/Hiscores"> Hiscores </Link>
                 </li>
             </ul>
+            <div id = "moveCircle" onClick = {setXY}>
+                move circle clicking this text
+            </div>
             <div id = "CursorCircle">
 
                 <div id = "InnerCircle"></div>
+
             </div>
         </div>
     )
