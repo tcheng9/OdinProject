ce61a24c321559121206206508fae7bf0efa60b9
tcheng9 <tcheng789@gmail.com>
2022-07-19 22:17:33 -0500

    update

--- wheres-waldo/src/components/WaldoMain.js
+++ wheres-waldo/src/components/WaldoMain.js
@@ -4,8 +4,8 @@ import './WaldoMain.css';
 import {useState, useEffect} from 'react';
 
 const WaldoMain = () => {
-    const [x, setX] = useState();
-    const [y, setY] = useState();
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
     const [seconds, setSeconds] = useState(0);
     function setXY() {
         
@@ -24,30 +24,43 @@ const WaldoMain = () => {
         console.log("Original" + e.screenX + ' ----------' + e.screenY);
         console.log("X coordinate: " + (e.screenX - 50));
         console.log("Y coordinate: " + (e.screenY - 150));
-        checkWaldo(e, e.screenX, e.screenY);
+        setX(e.screenX - 50);
+        setY(e.screenY - 150);
     }
 
-    const checkWaldo = (e,x,y) => {
-        //Waldo's coordinates
-        let waldoCoords = [100, 100]; 
+    // const checkWaldo = (e,x,y) => {
+    //     //Waldo's coordinates
+    //     let waldoCoords = [100, 100]; 
 
-        //Get coordinates from setCircle/ on trigger. Don't use screen listener
+    //     //Get coordinates from setCircle/ on trigger. Don't use screen listener
         
-        //If x and y are within range of waldo's coordinates, trigger a popup that lets user input their name
-        if ((x < waldoCoords[0] + 100 && x > waldoCoords[0] - 100) && (y > waldoCoords[1] - 100 && y < waldoCoords[1] + 100)){
-            console.log("correct coordinates")
-        }
-    }
+    //     //If x and y are within range of waldo's coordinates, trigger a popup that lets user input their name
+    //     if ((x < waldoCoords[0] + 100 && x > waldoCoords[0] - 100) && (y > waldoCoords[1] - 100 && y < waldoCoords[1] + 100)){
+    //         console.log("correct coordinates")
+    //     }
+    // }
     
+
+
+
     useEffect(() => {
+
+        
         const interval = setInterval(() => {
-            setSeconds(seconds => seconds + 1);     
-            }, 1000);
+            setSeconds(seconds => seconds + 1);  
             
+
+        }, 1000);
+
+        if (x > 50 && y > 50){
             
-            return () => clearInterval(interval);
+        }
+
+        return () => clearInterval(interval);
+
     }, [seconds]);
 
+  
     return (
         <div>
             
@@ -70,6 +83,10 @@ const WaldoMain = () => {
             
             <div>
                 {seconds} second has past
+            </div>
+
+            <div id = "winnerForm">
+                
             </div>
         </div>
     )
